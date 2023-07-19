4d988772785caeba49681e5ba5a43987e2884fda
David R <davrodzam@gmail.com>
2023-07-19 11:05:37 -0600

    CardContainer with arguments

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,11 +8,23 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: AuthBackgound(
-        child: Container(
-          width: double.infinity,
-          height: 300,
-          color: Colors.red,
-        ),
+        child: SingleChildScrollView(
+            child: Column(
+          children: [
+            SizedBox(height: 250),
+            CardContainer(
+              child: Column(
+                children: [
+                  const SizedBox(height: 10),
+                  Text('Login',
+                      style: Theme.of(context).textTheme.headlineSmall),
+                  const SizedBox(height: 10),
+                  const Text('Formulario')
+                ],
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
