0f96a606b0eba19e88ce7522b910e966bfa2ce02
David R <davrodzam@gmail.com>
2023-07-21 13:38:51 -0600

    email&password -Form Validation

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -28,7 +28,8 @@ class LoginScreen extends StatelessWidget {
             const Text(
               'Crear una nueva cuenta',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            )
+            ),
+            const SizedBox(height: 50),
           ],
         )),
       ),
@@ -42,44 +43,64 @@ class _Formulario extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 1, vertical: 1),
       child: Form(
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Column(
-        children: [
-          TextFormField(
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecorations.authInputDecoration(
-                hintText: 'email.address@gmail.com',
-                labelText: 'Correo electronico',
-                prefixIcon: Icons.alternate_email_rounded),
-          ),
-          const SizedBox(height: 30),
-          TextFormField(
-              autocorrect: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecorations.authInputDecoration(
-                hintText: 'Password',
-                labelText: 'Contraseña',
-                prefixIcon: Icons.lock_outline,
-              )),
-          const SizedBox(height: 50),
-          MaterialButton(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            color: Colors.deepPurple,
-            elevation: 0,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-              child: const Text(
-                'Ingresar',
-                style: TextStyle(fontSize: 18, color: Colors.white),
+            children: [
+              TextFormField(
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: 'email.address@gmail.com',
+                    labelText: 'Correo electronico',
+                    prefixIcon: Icons.alternate_email_rounded),
+                validator: (value) {
+                  String pattern =
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                  RegExp regExp = new RegExp(pattern);
+
+                  return regExp.hasMatch(value ?? '')
+                      ? null
+                      : 'Ingrese un correo';
+                },
               ),
-            ),
-            onPressed: () {},
-          ),
-          const SizedBox(height: 50),
-        ],
-      )),
+              const SizedBox(height: 30),
+              TextFormField(
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: '*******',
+                  labelText: 'Password',
+                  prefixIcon: Icons.lock_outline,
+                ),
+                validator: (value) {
+                  return (value != null && value.length >= 6)
+                      ? null
+                      : 'Password must be 6 characters';
+                },
+              ),
+              const SizedBox(height: 50),
+              MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                color: Colors.deepPurple,
+                elevation: 0,
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                  child: const Text(
+                    'Ingresar',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                ),
+                onPressed: () {
+                  //TODO: Login
+                },
+              ),
+              const SizedBox(height: 50),
+            ],
+          )),
     );
   }
 }
