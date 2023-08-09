27d4f3d6052eea2d0ce6265eb59f9726ee715885
David R <davrodzam@gmail.com>
2023-08-09 09:25:00 -0600

    Widget - ProductCard - borderRadius

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -10,7 +10,57 @@ class ProductCard extends StatelessWidget {
         margin: const EdgeInsets.only(top: 30, bottom: 50),
         width: double.infinity,
         height: 400,
-        color: Colors.red,
+        decoration: _cardBorders(),
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: [
+            _BackgroundImage(),
+            _ProductDetails(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _cardBorders() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black,
+              offset: Offset(0, 7),
+              blurRadius: 10,
+            )
+          ]);
+}
+
+class _ProductDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(25), bottomRight: Radius.circular(25)),
+      child: Container(
+        width: double.infinity,
+        height: 70,
+        color: Colors.indigo,
+      ),
+    );
+  }
+}
+
+class _BackgroundImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: Container(
+        width: double.infinity,
+        height: 400,
+        child: const FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/400x300'),
+        ),
       ),
     );
   }
